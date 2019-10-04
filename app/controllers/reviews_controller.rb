5950088b9809ff93066f114c23a11a1379c8360a
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-04 16:16:12 +0900

    made review's model and controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,6 @@
 class ReviewsController < ApplicationController
+
+  def index
+  end
+
 end
--- db/migrate/20191004065812_create_reviews.rb
+++ db/migrate/20191004065812_create_reviews.rb
@@ -1,7 +1,11 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
-
+      t.string :title, null: false, unique: true, index: true
+      t.string :family_name, null: false
+      t.string :first_name
+      t.string :place
+      t.text   :body
       t.timestamps
     end
   end
