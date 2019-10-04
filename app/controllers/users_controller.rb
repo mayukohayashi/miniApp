0a8212b15e2ec489677592b96c424c3f1fe5b152
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-04 20:20:57 +0900

    done application view page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
 
-  def index
+  def show
+    @user = User.find(params[:id])
+    @nickname = user.name
+    @reviews = user.reviews.page(params[:page]).per(5).order('created_at DESC')
   end
 
+  private
+  def show_params
+    params.permit(:id)
+  end
 end
