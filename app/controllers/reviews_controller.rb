393dc36aa064c916c88e11a9812d7014d4bb7c9d
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-06 12:26:52 +0900

    done!?

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,34 +1,36 @@
 class ReviewsController < ApplicationController
-before_action :redirect_to_index, except: :index
+  # before_action :redirect_to_index, :except => [:index]
 
-def index
-  @reviews = Review.includes(:user).page(params[:page]).per(10).order("created_at DESC")
-end
+  def index
+    @reviews = Review.includes(:user).page(params[:page]).per(5).order("created_at DESC")
+  end
 
-def show
-  @review = Reviews.find(params[:id])
-end
+  def new
+    @review = Review.new
+  end
 
-def new
-  @review = Review.new
-end
+  def create
+    @review = Review.create(review_params)
+    redirect_to root_path
+  end
 
-def create
-  review = Review.create(create_params)
-  redirect_to_index
-end
+  def destroy
+    review.destroy if review.user_id == current_user.id
+  end
 
-def edit
-  @review = Review.find(id_params[:id])
-end
+  def edit
+    @review = Review.find(id_params[:id])
+  end
 
-def update
-  review = Review.find(id_params[:id])
-  review.update(review_params) if review.user_id == current_user.id
-end
+  def update
+    review = Review.find(id_params[:id])
+    review.update(review_params) if tweet.user_id == current_user.id
+  end
 
-private
-  def create_params
+
+  private
+
+  def review_params
     params.require(:review).permit(:title, :family_name, :first_name, :place, :body).merge(user_id: current_user.id)
   end
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
   validates :title, :body, presence:true
   belongs_to :user
+
 end
