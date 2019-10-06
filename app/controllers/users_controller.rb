a7959e9da34775d776832ce40bd90741755607da
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-06 14:46:30 +0900

    start to make it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
 
+  def index
+  end
+
   def show
-    @user = User.find(params[:id])
-    # @nickname = user.name
-    @reviews = user.reviews.page(params[:page]).per(5).order('created_at DESC')
+    # @user = User.find(params[:id])
+    # # @nickname = user.name
+    # @reviews = user.reviews.page(params[:page]).per(5).order('created_at DESC')
   end
 
   private
